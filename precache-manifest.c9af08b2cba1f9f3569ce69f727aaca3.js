9313eb36139c9d3cd6c0dac1fb7ec43a1f5b7d0e
Haoyang Liu <haoyang.liu@uqconnect.edu.au>
2020-05-10 20:35:36 +1000

    Updates

---
+++ precache-manifest.c9af08b2cba1f9f3569ce69f727aaca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9747c8eadfc49e72d264dbd315ce16d4",
+    "url": "/weather-app-modify/index.html"
+  },
+  {
+    "revision": "cf72ca54267756356716",
+    "url": "/weather-app-modify/static/css/main.1b7c73a8.chunk.css"
+  },
+  {
+    "revision": "ff0ca7586714bf02cb02",
+    "url": "/weather-app-modify/static/js/2.91a54acc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-modify/static/js/2.91a54acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf72ca54267756356716",
+    "url": "/weather-app-modify/static/js/main.79fb771d.chunk.js"
+  },
+  {
+    "revision": "8fa0305db1b11b40f12b",
+    "url": "/weather-app-modify/static/js/runtime-main.2344bd1a.js"
+  }
+]);
